46d1e94883a90461d1ed548bf8dcdd96441daf93
shah8003 <harisahmed760@gmail.com>
2023-08-06 13:41:17 +0500

    Update crossOrigin annotation

--- src/main/java/com/example/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/com/example/springbootecommerce/dao/ProductRepository.java
@@ -2,8 +2,8 @@ package com.example.springbootecommerce.dao;
 
 import com.example.springbootecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category" )
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
